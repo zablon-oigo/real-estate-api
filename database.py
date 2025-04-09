64bb15f525d291f70a0c555f242555af04d55250
zablon <zablonoigo51@gmail.com>
2025-04-09 09:08:31 +0300

    Add enviroment vars

--- database.py
+++ database.py
@@ -3,8 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
+from .config import get_settings
 
-
+settings=get_settings()
+DATABASE_URL=f"postresql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
