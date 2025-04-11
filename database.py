3dfa7b39e2fb7b28b5297680d1be600a193256fc
zablon <zablonoigo51@gmail.com>
2025-04-11 23:01:04 +0300

    fix typo

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import get_settings
 
 settings=get_settings()
-DATABASE_URL=f"postresql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+DATABASE_URL=f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
