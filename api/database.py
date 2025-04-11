33f17070f25369bdad59cfc4a62b123924f968ba
zablon <zablonoigo51@gmail.com>
2025-04-11 23:47:55 +0300

    root folder

---
+++ api/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+from .config import get_settings
+
+settings=get_settings()
+DATABASE_URL=f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+engine=create_engine(DATABASE_URL)
+SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+
+
+Base=declarative_base()
+
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+
+    
