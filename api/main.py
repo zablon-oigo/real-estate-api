29d00be104affd549e617576c1d212ca792a251a
zablon <zablonoigo51@gmail.com>
2025-04-13 12:59:02 +0300

    Fix typo

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from .routers import auth
 app=FastAPI(
     title="Real Estate API",
     description="Property listing API",
@@ -10,3 +10,4 @@ async def root():
     return {"message": "Hello World"}
 
 
+app.include_router(auth.router)
