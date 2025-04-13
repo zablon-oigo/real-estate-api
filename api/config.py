98a6ac69a62e23f66ea8820155b94ee19a950836
zablon <zablonoigo51@gmail.com>
2025-04-13 12:58:16 +0300

    Add config file

--- api/config.py
+++ api/config.py
@@ -1,23 +1,22 @@
 import pathlib
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings(BaseSettings):
-    db_host:str
-    db_port:int 
-    db_username:str 
-    db_password:str 
-    db_name:str 
-
+    db_host: str
+    db_port: int
+    db_username: str
+    db_password: str
+    db_name: str
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_server: str
 
     class Config:
-        env_file=f"{pathlib.Path(__file__).resolve().parent}/.env"
-
+        env_file = f"{pathlib.Path(__file__).resolve().parent}/.env"
 
 def get_settings():
     return Settings()
-
-
-get_settings()
