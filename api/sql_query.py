d162d87c9c6bdc29614cd200bf5934107faf2096
zablon <zablonoigo51@gmail.com>
2025-04-17 22:40:35 +0300

    Add sql query

---
+++ api/sql_query.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+from . import models, schema
+from pydantic import EmailStr
+
+
+def check_user_exist(db: Session, email: EmailStr):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def insert_new_user(db: Session, user: schema.UserCreate):
+    new_user = models.User(
+        email=user.email,
+        password=user.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+def create_otp_for_user(db: Session, otp: schema.OTPData):
+    new_otp = models.UserOneTimePassword(
+        user_id=otp.user_id,
+        code=otp.code
+    )
+    db.add(new_otp)
+    db.commit()
+    db.refresh(new_otp)
+    return new_otp
