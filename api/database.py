14ef1f40a1e50df15740e1d14d6027055d27a176
zablon <zablonoigo51@gmail.com>
2025-04-18 22:11:24 +0300

    Add cloudinary

--- api/database.py
+++ api/database.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import cloudinary
 
 
 from .config import get_settings
 
 settings=get_settings()
+cloudinary.config(
+    cloud_name=settings.cloud_name,
+    api_key=settings.cloud_api_key,
+    api_secret=settings.cloud_secret,
+    secure=True
+)
+
 DATABASE_URL = (
     f"postgresql://{settings.db_username}:{settings.db_password}"
     f"@{settings.db_host}:{settings.db_port}/{settings.db_name}"
@@ -24,4 +32,3 @@ def get_db():
     finally:
         db.close()
 
-    
